4dc5515800f3e3ecfa4fc94703ba62bef335175e
Elard Guevara <elard.guevara@ucsp.edu.pe>
2016-08-31 13:50:06 -0500

    subiendo

--- multiplicacion.c
+++ multiplicacion.c
@@ -18,12 +18,12 @@ void llenar(int m[TAM][TAM])
 
 void print(int m[TAM][TAM])
 {
-  	for (int i=0; i<TAM; ++i) 
-  	{
+    for (int i=0; i<TAM; ++i) 
+    {
         for (int j=0; j<TAM; ++j)
             printf("%d\t", m[i][j]);
         printf("\n");
-  	}
+    }
     printf("\n");
 }
 
@@ -39,35 +39,32 @@ int main(int argc, char const *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
     if(my_rank==0)
-  	{
+    {
         fill(A);
         fill(B);
-
         print(A);
         print(B);
-  	}
+    }
 
     int ini = my_rank*TAM/comm_sz;
-  	int end = (my_rank+1)*TAM/comm_sz;
+    int end = (my_rank+1)*TAM/comm_sz;
 
-  	MPI_Bcast (B, TAM*TAM, MPI_INT, 0, MPI_COMM_WORLD);
-  	MPI_Scatter (A, TAM*TAM/comm_sz, MPI_INT, A[ini], TAM*TAM/comm_sz, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast (B, TAM*TAM, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter (A, TAM*TAM/comm_sz, MPI_INT, A[ini], TAM*TAM/comm_sz, MPI_INT, 0, MPI_COMM_WORLD);
 
-  	for (int i = ini; i < end; ++i)
-  	{ 
-    	  for (int j = 0; j < TAM; ++j) 
-    	  {
-    		    for (int k = 0; k < TAM; ++k)
-				    C[i][j]+=A[i][k]*B[k][j];
-    	  }
+    for (int i = ini; i < end; ++i)
+    { 
+        for (int j = 0; j < TAM; ++j) 
+        {
+            for (int k = 0; k < TAM; ++k)
+                C[i][j]+=A[i][k]*B[k][j];
+        }
     }
 
     MPI_Gather(C[ini], TAM*TAM/comm_sz, MPI_INT, C, TAM*TAM/comm_sz, MPI_INT, 0, MPI_COMM_WORLD);
 
     if (my_rank==0)
-    {
-    	  print(C);
-    }
+        print(C);
 
     MPI_Finalize();
 
